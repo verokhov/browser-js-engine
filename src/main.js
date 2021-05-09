4c3f228bc09231de323fc2a814d9a4da9343c7c5
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2021-05-09 17:36:59 +0700

    Added base url for router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
+import { BASE_URL } from '@/config';
+
 import App from './App.vue';
 import routes from './routes';
 
 import './assets/styles/main.scss';
 
 const router = createRouter({
+  base: BASE_URL,
   history: createWebHistory(),
   routes,
 });
